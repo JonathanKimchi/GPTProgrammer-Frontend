2c5ee1c8262860fb299bf8fa117e5fb170524d2d
JonathanKimchi <59551654+JonathanKimchi@users.noreply.github.com>
2023-03-12 18:44:41 -0400

    Change Backend hosting URL

--- src/client/codeGeneratorApiService.js
+++ src/client/codeGeneratorApiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const isDevelopment = process.env.NODE_ENV == null || process.env.NODE_ENV === 'development';
 
-export const baseUrl = isDevelopment ? 'http://localhost:4242' : 'https://23.21.86.81:443';
+export const baseUrl = isDevelopment ? 'http://localhost:4242' : 'https://appgpt-backend.com';
 
 console.log('selecting baseUrl: ' + baseUrl);
 
