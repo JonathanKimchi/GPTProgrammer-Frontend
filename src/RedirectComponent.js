6fc724965fbc9e7974968965a650180a089af3d1
JonathanKimchi <59551654+JonathanKimchi@users.noreply.github.com>
2023-05-11 20:55:18 -0400

    Adding timeout for Redirect

--- src/RedirectComponent.js
+++ src/RedirectComponent.js
@@ -2,7 +2,11 @@ import React, { useEffect } from 'react';
 
 const RedirectComponent = () => {
   useEffect(() => {
-    window.location.href = 'https://forms.gle/vPDA4xAzgoiGNtZf8';
+    const timer = setTimeout(() => {
+      window.location.href = 'https://forms.gle/vPDA4xAzgoiGNtZf8';
+    }, 3000); // Wait for 3 seconds before redirecting
+
+    return () => clearTimeout(timer); // Clean up the timer when the component unmounts
   }, []);
 
   return <div>Redirecting to Google Form...</div>;
